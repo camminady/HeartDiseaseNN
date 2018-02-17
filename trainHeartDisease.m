340eed458888b0a29c520697854dcc00c07a8182
Thomas <camminady@mathcces.rwth-aachen.de>
2018-02-17 17:39:08 +0100

    Output formatting

--- trainHeartDisease.m
+++ trainHeartDisease.m
@@ -3,9 +3,8 @@ clc
 clearvars
 close all
 
-trainpercentage = 0.8;
+trainpercentage = 0.7;
 [inputtrain, targettrain, inputtest, targettest] = splitDataset(trainpercentage);
-
 
 layers = [13,1];
 net = fitnet(layers);
@@ -16,12 +15,14 @@ net = fitnet(layers);
 % errtrain = norm(predictiontrain-targettrain)/norm(targettrain)
 
 predictiontest = net(inputtest);
+predictiontrain = net(inputtrain);
 
 % let us say our net is certain someone has the disease if the certainty is
 % above certain treshold
 % and let us  take a low treshold (bc we rather want a false positive.
-treshold = 0.4;
+treshold = 0.5;
 predictiontestbinary = predictiontest>treshold;
+predictiontrainbinary = predictiontrain>treshold;
 
 
 figure;markersize = 10;
@@ -36,10 +37,38 @@ ylabel('has disease, yes/no');
 
 
 % 4 felder tafel wohooo
-a = sum(targettest & predictiontestbinary);
-b = sum(not(targettest) & predictiontestbinary);
-c = sum(targettest & not(predictiontestbinary));
-d = sum(not(targettest) & not(predictiontestbinary));
+fprintf('Test\n');
+print4FelderTafel(targettest,predictiontestbinary)
+fprintf('Train\n');
+print4FelderTafel(targettrain,predictiontrainbinary)
 
-fprintf('\t\t\thas disease\thas no disease\nclassified disease\t%f\t%f\nclassified healthy\t%f\t%f\n',a,b,c,d)
+
+
+
+
+end
+
+
+function print4FelderTafel(target,perdiction)
+% 4 felder tafel wohooo
+a = sum(target & perdiction);
+b = sum(not(target) & perdiction);
+c = sum(target & not(perdiction));
+d = sum(not(target) & not(perdiction));
+n = length(target);
+s = sum(target)/n;
+h = 1-s;
+fprintf('prediction of net:\n');
+fprintf(' N=%i\t\t\thas disease\thas no disease\nclassified disease\t%f\t%f\nclassified healthy\t%f\t%f\n\n',n,a,b,c,d)
+fprintf('if guessing:\n');
+fprintf(' N=%i\t\t\thas disease\thas no disease\nclassified disease\t%f\t%f\nclassified healthy\t%f\t%f\n\n',n,s*s*n,s*h*n,h*s*n,h*h*n)
+fprintf('\n##########################################################\n');
+
+
+
+
+end
+
+
+
 
